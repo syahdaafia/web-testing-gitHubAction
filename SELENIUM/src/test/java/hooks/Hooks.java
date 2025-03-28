b546493031c0653a1b20b653735235daca54f7d8
Syahda Afia <syahdaafia@gmail.com>
2025-03-28 19:57:59 +0700

    Update Hooks.java

--- SELENIUM/src/test/java/hooks/Hooks.java
+++ SELENIUM/src/test/java/hooks/Hooks.java
@@ -1,6 +1,37 @@
-ChromeOptions options = new ChromeOptions();
-options.addArguments("--headless"); // Menjalankan tanpa GUI
-options.addArguments("--disable-gpu"); // Opsional, untuk beberapa sistem operasi
-options.addArguments("--window-size=1920,1080"); // Opsional, agar resolusi tetap stabil
+package hooks;
 
-driver = new ChromeDriver(options);
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Hooks {
+    private static WebDriver driver;
+    private static final String BASE_URL = "https://www.saucedemo.com/";
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Jalankan tanpa GUI
+        options.addArguments("--disable-gpu"); // Opsional
+        options.addArguments("--window-size=1920,1080"); // Opsional
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get(BASE_URL);
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+}
